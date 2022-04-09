f6a86fd7a4afa0248db7e8152a27ce22afa4b847
sleem3232 <sleem3232@gmail.com>
2022-04-09 06:25:47 +0300

    Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Routes,Route,Link,useParams} from 'react-router-dom'
+import Users from './Users';
+const Homepage=()=>{
+  return( <div>this is home</div>)
+}
+const Aboutus=()=>{
+  return( <div>aboutus</div>)
+}
+const Itemsall=()=>{
+  let {name}=useParams();
+  return(
+    <div>
+      <h1>Items</h1>
+      <div>{name}</div>
+    </div>
+  )
+}
 
+const Items=()=>{
+  return( <div>Items
+    <div>
+      <ul>
+  <li>1</li>
+  <li>2</li>
+  <li>3</li>
+  <li>4</li>
+  <li>5</li>
+  </ul>
+  </div>
+  </div>
+  )
+}
+
+ 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+     <BrowserRouter>
+   
+    <ul> <Link to="/Home">home</Link></ul>
+    <ul> <Link to="/Items">Items</Link></ul>
+    <ul>   <Link to="/Aboutus">Aboutus</Link></ul>
+    <ul>   <Link to="/Users">Users</Link></ul>
+     <Routes>
+      
+       <Route path="/Home" element={<Homepage/>}></Route>
+       <Route path="/users" element={<Users/>}></Route>
+
+       <Route path="/Items" element={<Items/>}></Route>
+       <Route path="/Aboutus" element={<Aboutus/>}></Route>
+     
+      
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
